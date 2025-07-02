50a7425709195f18d3438a89f1252e8b2ae520a2
cmama <cmama@cmamas-MacBook-Air.local>
2025-07-02 15:01:51 +0200

    com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import markdoc from '@astrojs/markdoc';
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ajaibghar-co.github.io",
+  base: "ajaibghar-landing-page",
   integrations: [
     markdoc({
-      allowHTML: true
-    })
+      allowHTML: true,
+    }),
   ],
   vite: {
     plugins: [tailwindcss()],
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,13 @@
 import { z, defineCollection } from "astro:content";
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 
-  
 export const workPosts = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/data/banner" }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string()
-    }),
-    // type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/workPosts/" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+  // type: "content",
 });
 
 export const collections = { workPosts };
